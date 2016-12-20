1a92f0cc52d7a66bc4943881715b717936fc8361
Ricard Sole <ricardsolecasas@gmail.com>
2016-12-20 22:32:32 +0100

    uses correct configuration for production db

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule PhoenixPeople.Mixfile do
 
   def project do
     [app: :phoenix_people,
-     version: "1.0.0",
+     version: "1.0.1",
      elixir: "~> 1.2",
      elixirc_paths: elixirc_paths(Mix.env),
      compilers: [:phoenix, :gettext] ++ Mix.compilers,
