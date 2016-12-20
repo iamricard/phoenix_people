f88a29772630c1e9461f1e5221ae74801d8ea12c
Ricard Sole <ricardsolecasas@gmail.com>
2016-12-20 22:21:29 +0100

    version

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule PhoenixPeople.Mixfile do
 
   def project do
     [app: :phoenix_people,
-     version: "0.0.1",
+     version: "1.0.0",
      elixir: "~> 1.2",
      elixirc_paths: elixirc_paths(Mix.env),
      compilers: [:phoenix, :gettext] ++ Mix.compilers,
